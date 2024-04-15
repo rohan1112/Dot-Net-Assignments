8a4659d655628eb2205467860b39198049b874ce
rohan-1112 <rohankalaskar1112@gmail.com>
2024-04-15 20:19:19 +0530

    Day 4 Assignments

---
+++ Day 4/StringTask/Program.cs
@@ -0,0 +1,24 @@
+﻿// Q1. Create 2 string variable and accept data from user.
+// Using equals and == check data entered by user is same or not.
+// Print gethashcode for both string .variable
+// Also use referenceEquals and observe out put.
+
+class Program{
+
+    static void Main(string [] args){
+    string str1;
+    string str2;
+
+    str1=Console.ReadLine();
+    str2=Console.ReadLine();
+
+    Console.WriteLine(str1.Equals(str2));
+    Console.WriteLine(str1==str2);
+    Console.WriteLine(str1.GetHashCode());
+    Console.WriteLine(str2.GetHashCode());
+    Console.WriteLine(Object.ReferenceEquals(str1,str2));
+    Console.ReadLine();
+    }
+
+
+}
